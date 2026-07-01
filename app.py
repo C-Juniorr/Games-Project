31ef78642e626ec3fcfdad355b1a33e3b0b5e924
Clodoaldo Junior <clodoaldo@gmail>
2026-07-01 10:03:25 -0300

    V1 Sistema Games Clodoaldo

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/ultimate")
+def ultimate():
+    return render_template("tictactoe.html")
+
+@app.route("/reinos")
+def quiz():
+    return render_template("reino_mobile.html")
+
+@app.route("/football")
+def memory():
+    return render_template("football-master.html")
+
+@app.route("/reigns-H.O.T.D")
+def reignshotd():
+    return render_template("trono_de_fogo.html")
+
+app = app
+"""if __name__ == "__main__":
+    app.run(debug=True)"""
